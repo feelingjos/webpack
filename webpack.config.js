0307925b8f362cf4bd6f69de45b98dce1e323e02
feelj <feelingjos@163.com>
2019-09-23 22:34:35 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
 const HtmlInlinkChunkPlugin = require('html-webpack-inline-chunk-plugin')
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
 
 module.exports = {
 	mode: 'development',
@@ -264,6 +241,7 @@ module.exports = {
 	},
 
 	devServer: {
-		open: true
+		open: true,
+		port: 9001
 	}
 };
