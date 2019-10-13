aa685b0b78699ec870561a93031eb299e8f19a7e
feelj <feelingjos@163.com>
2019-10-13 22:38:18 +0800

    拖拽

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ export default (function(){
     const tableResize = document.getElementsByClassName("table-header-right-resize")
 
     for (var num = 0; num <= tableResize.length; num ++) {
-        tableResize[0].onclick = table_cell_right_resize
+        const doms = tableResize[0];
+
+        //doms.onclick = table_cell_right_resize
+
+        doms.onmousemove = function (ev){
+
+            var oEvent = ev || event
+
+            console.log("哈哈")
+
+            //console.log('clientX',oEvent.clientX)
+
+            //console.log('offsetX',oEvent.offsetX)
+
+            //console.log(ev)
+
+            //doms.parentNode.style.width  += oEvent.offsetX
+        }
+
+        doms.onmouseup=function (ev) {
+            console.log("呵呵")
+        }
 
 
     }
--- src/scripts/table.js
+++ src/scripts/table.js
@@ -1,9 +1,43 @@
+//点击事件
 const table_cell_right_resize = (x) =>{
 
     console.log(x)
 
 }
 
+var mouseStart = {};
+var rightStart = {};
+
+const table_cell_rigth_doresize = (ev) =>{
+    var oEvent = ev || event;
+    var l = oEvent.clientX - mouseStart.x + rightStart.x;
+    var w = l + oDiv.offsetWidth;
+
+    if (w < oDiv.offsetWidth) {
+        w = oDiv.offsetWidth;
+    }
+    else if (w > document.documentElement.clientWidth - oDiv2.offsetLeft) {
+        w = document.documentElement.clientWidth - oDiv2.offsetLeft - 2;
+    }
+}
+
+const table_cell_rigth_stopresize = (ev) =>{
+    if (right.releaseCapture) {
+        right.onmousemove = null;
+        right.onmouseup = null;
+        right.releaseCapture();
+    }
+    else {
+        document.removeEventListener("mousemove", table_cell_rigth_doresize, true);
+        document.removeEventListener("mouseup", table_cell_rigth_stopresize, true);
+    }
+}
+
+
+
+
 module.exports = {
-    table_cell_right_resize
+    table_cell_right_resize,
+    table_cell_rigth_doresize,
+    table_cell_rigth_stopresize
 }
