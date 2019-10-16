3335240a63c75326ba2f1b725be5ef0ff0c7dccf
feelj <feelingjos@163.com>
2019-10-16 20:21:13 +0800

    统一行的放大与缩小

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
 import {table_cell_right_resize} from './src/scripts/table.js'
 import './src/styles/index.scss'
+//import './src/index2'
 
 export default (function(){
 
     const tableResize = document.getElementsByClassName("table-header-right-resize")
 
-    console.log(tableResize.length)
-    console.log(tableResize)
+    const cells = document.querySelectorAll("[data~=cell-1]")
 
-    for (var num = 0; num <= tableResize.length; num ++) {
-        const doms = tableResize[num];
+    for (var num = 0; num < tableResize.length; num ++) {
+        const doms = tableResize[num]
+        const parentdoms = doms.parentNode
 
-        const parent = doms.parentNode;
-
-        //console.log(parent)
+        var mouseStart = {}
+        var rightStart = {}
 
         doms.onmousedown = function(evdown){
 
-            // 阻止冒泡,避免缩放时触发移动事件
-           /* evdown.stopPropagation()
-            evdown.preventDefault()*/
-            var pos = {
-                'w': parent.offsetWidth,
-                'h': parent.offsetHeight,
-                'x': evdown.clientX,
-                'y': evdown.clientY
+            evdown.stopPropagation()
+            evdown.preventDefault()
+
+            var oEvent = evdown || event
+            mouseStart.x = oEvent.clientX
+            mouseStart.y = oEvent.clientY
+            rightStart.x = doms.offsetLeft
+
+            if(doms.setCapture){
+                doms.onmousemove = doDrag1
+                doms.onmouseup = stopDrag1
+                doms.setCapture()
+            }else{
+                document.addEventListener("mousemove",doDrag1,true)
+                document.addEventListener("mouseup",stopDrag1,true)
             }
 
-            //console.log(pos)
+        }
 
-            doms.onmousemove = function(evmove){
+        function doDrag1(ev){
 
-                /*evmove.stopPropagation()
-                evmove.preventDefault()*/
+            var oEvent = ev || event
 
-                const dragSize = evmove.clientX - pos.x
+            var l = oEvent.clientX - mouseStart.x + rightStart.x
+            var w = l + doms.offsetWidth
 
-                const width = parent.clientWidth||parent.offsetWidth
+            if(w < doms.offsetWidth){
 
-                console.log(dragSize)
+                w = doms.offsetWidth
 
-                parent.style.width = width + dragSize + 'px'
+            }else if( w > document.documentElement.clientWidth - parentdoms.offsetLeft){
+                w = document.documentElement.clientWidth - parentdoms.offsetLeft - 2
+            }
 
-                pos.x = evmove.clientX
+
+            for (var ci = 0 ; ci <= cells.length; ci ++){
+                if(ci < cells.length){
+                    cells[ci].style.width = w + 'px'
+                }else if (ci = cells.length){
+                    parentdoms.style.width = w + "px"
+                }
 
             }
 
-            /*doms.onmouseleave = function () {
-                doms.onmousemove=null;
-                doms.onmouseup=null;
-            }*/
 
-            doms.onmouseup = function(){
+        }
 
+        function stopDrag1(){
+            if(doms.releaseCapture){
                 doms.onmousemove = null
                 doms.onmouseup = null
+                doms.releaseCapture()
+            }else{
+                document.removeEventListener("mousemove",doDrag1,true)
+                document.removeEventListener("mouseup",stopDrag1,true)
             }
-
-
-
-
-
         }
 
 
